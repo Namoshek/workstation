5980c7e35fd7dd436e3e96f307b0393c0bc7734e
Namoshek <Namoshek@users.noreply.github.com>
2018-05-25 17:09:26 +0200

    Create 1_install_choco_pkgs.ps1

---
+++ scripts/1_install_choco_pkgs.ps1
@@ -0,0 +1,51 @@
+# =====================================================================
+# This script installs packages via chocolatey.
+# =====================================================================
+
+ECHO Configuring chocolatey
+choco feature enable -n allowGlobalConfirmation
+
+ECHO Installing office applications
+choco install googlechrome
+choco install firefox
+choco install jre8
+choco install notepadplusplus
+choco install discord
+choco install slack
+choco install 7zip
+choco install winrar
+choco install pdf24
+choco install office365business
+choco install vlc
+
+ECHO Installing developer applications
+choco install virtualbox
+choco install vboxguestadditions.install
+choco install vagrant
+choco install git
+choco install github
+choco install winscp
+choco install nodejs
+choco install yarn
+choco install postman
+choco install etcher
+choco install dotnetcore-sdk
+choco install visualstudiocode
+choco install visualstudio2017community
+choco install jdk8
+choco install sql-server-2017
+choco install sql-server-management-studio
+choco install python
+choco install miktex
+choco install texstudio
+choco install netbeans
+choco install phpstorm
+
+ECHO Installing gaming applications
+choco install steam
+choco install origin
+choco install uplay
+choco install msiafterburner
+
+ECHO Configuring chocolatey
+choco feature disable -n allowGlobalConfirmation
